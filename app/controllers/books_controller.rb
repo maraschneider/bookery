018951751fd90bd74f83505c947f701aea68e8e0
ramiro gordillo <ramirogordillo@hotmail.com>
2019-11-19 17:50:18 +0100

    Finished feaure: Book Search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,17 @@
 class BooksController < ApplicationController
-
   def index
-
   end
 
   def show
     @book = Book.find(params[:id])
-    @rental = Rental.new()
+    @rental = Rental.new
+  end
 
+  def search
+    if params[:title].present?
+      @title = params[:title]
+      @books = Book.where("title ILIKE ?", "%#{@title}%")
+    end
   end
 
   def show_selection
