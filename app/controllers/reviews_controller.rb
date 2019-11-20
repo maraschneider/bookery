0a9c0170b1eccc8c2b56eb7b85361c9a903716d9
Sharina Alves <sharina.alves@mail.de>
2019-11-20 21:30:45 +0100

    add policy to review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,24 @@
 class ReviewsController < ApplicationController
   def new
+    @review = Review.new
    # @review = current_user.rentals.reviews.new
-   # authorize @review
+   authorize @review
   end
 
   def create
+    @review = Review.new(review_params)
   #  @review = current_user.rentals.reviews.new(review_params)
-  #  authorize @review
-  #  if @review.save
-  #    redirect_to @review
-  #  else
-  #    render 'new'
-  #  end
+  authorize @review
+    if @review.save
+      redirect_to @review
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
   end
 end
---
+++ app/policies/review_policy.rb
@@ -0,0 +1,29 @@
+class ReviewPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  # def show?
+  #   true
+  # end
+
+  # def update?
+  #  is_owner?
+  # end
+
+  def create?
+    user
+  end
+
+  # def destroy?
+  #  is_owner?
+  # end
+
+  private
+
+  # def is_owner?
+  #  record.user == user
+  # end
+end
