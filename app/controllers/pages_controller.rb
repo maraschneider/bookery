210ac9bb867359e01bb31ac64f4f62040b793dea
ramiro gordillo <ramirogordillo@hotmail.com>
2019-11-20 15:19:56 +0100

    Fixed sticky bar, controller variable, opacity

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
     @books = Book.all
-    @book = Book.new(params[:id])
+    @book = Book.new
   end
 end
