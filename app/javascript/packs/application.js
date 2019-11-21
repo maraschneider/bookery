6d8b934faa70073654a926296d3cdc9fb0506346
Sharina Alves <sharina.alves@mail.de>
2019-11-21 12:05:10 +0100

    add date picker to book page with a range

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,9 @@
 import "bootstrap";
+import flatpickr from "flatpickr";
+import "flatpickr/dist/themes/airbnb.css";
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
+
+flatpickr("#start-date", {
+  altInput: true,
+  "plugins": [new rangePlugin({ input: "#end-date" })]
+});
