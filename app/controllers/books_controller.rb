8ee8cd3365a1fa8bac68bac44493dfdad2e2c5cb
ramiro gordillo <ramirogordillo@hotmail.com>
2019-11-21 12:19:29 +0100

    Included geocode gem, added lat and long to user model, initialized geocode

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show, :search]
   before_action :set_book, only: [:show, :edit, :update, :destroy]
+
   def index
     @books = policy_scope(Book)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_164718) do
+ActiveRecord::Schema.define(version: 2019_11_21_111050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 2019_11_19_164718) do
     t.string "language"
     t.text "description"
     t.string "profile_pic", default: "https://cdn.designbyhumans.com/i/S0q2SgMDnZTMIquCovyU0uSSGP2U1OLM9LwY_VJDQ0MzC2MzoGyulZmpQQUQ6yRZGeoUWBkCAA.pr253160-2-2704838.jpg"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
