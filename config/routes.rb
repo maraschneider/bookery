91b5ba2749a60a55fdb126093d5d03e486e2a7f9
Maraike Schneider <maschn93+github@gmail.com>
2019-11-21 12:21:41 +0100

    adds  dashboard routes
    ...for settings, readings, hostings

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :rentals
   resources :books
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :update]
   get 'books/search/:title', to: 'books#search'
   get 'dashboard', to: 'users#dashboard'
+  get 'dashboard/hostings', to: 'users#list_hostings'
+  get 'dashboard/readings', to: 'users#list_readings'
+  get 'dashboard/settings', to: 'users#edit'
 end
